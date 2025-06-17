6fc8343c85f957176eac7744621ab4cb5876a904
Sysinfocus <info@sysinfocus.com>
2025-06-17 19:35:02 +0530

    DotNetRun tool created and pushed to NuGet

---
+++ src/DotNetRun/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+if (args.Length != 1) return;
+
+var temporaryFolder = GetTemporaryFolder();
+
+var file = args[0];
+if (!File.Exists(file))
+{
+    Console.WriteLine($"File '{file}' not found.");
+    return;
+}
+
+ReadCSFile(temporaryFolder, args[0]);
+
+Console.WriteLine();
+
+
+static string GetTemporaryFolder()
+{
+    var tempPath = Path.GetTempPath();
+    var newTempFolder = "DotNetRunScriptingFolder";
+    var path = Path.Combine(tempPath, newTempFolder);    
+    Directory.CreateDirectory(path);
+    return path;
+}
+
+static void ReadCSFile(string path, string projectFile)
+{    
+    var projectName = Path.GetFileNameWithoutExtension(projectFile);
+    var programFile = Path.Combine(path, projectName, "Program.cs");
+
+    var csproj = Path.Combine(path, projectName, projectName + ".csproj");
+    var csprojContent = File.ReadAllText(csproj);    
+
+    if (!File.Exists(csproj))
+    {
+        var pi = new ProcessStartInfo
+        {
+            WorkingDirectory = path,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            FileName = "dotnet",
+            Arguments = $"new console -n {projectName}"
+        };
+        Process.Start(pi);
+    }
+
+    var lines = File.ReadAllLines(projectFile);
+    HashSet<Package> packages = [];
+    var sb = new StringBuilder();
+    foreach (var line in lines)
+    {
+        if (line.StartsWith("#:"))
+        {
+            var package = line.Trim().Replace("#:package ", "").Split('@');
+            packages.Add(new(package[0], package[1]));
+
+            if (!csprojContent.Contains($"Include=\"{package[0]}\"") &&
+                !csprojContent.Contains($"Version=\"{package[1]}\""))
+            {
+                var addPackage = new ProcessStartInfo
+                {
+                    WorkingDirectory = path,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    FileName = "dotnet",
+                    Arguments = $"add package {package} --project ./{projectName}"
+                };
+                Process.Start(addPackage);
+            }
+        }
+        else
+        {
+            sb.AppendLine(line);
+        }
+    }
+
+    File.WriteAllText(programFile, sb.ToString());
+    var pix = new ProcessStartInfo
+    {
+        WorkingDirectory = path,
+        FileName = "dotnet",
+        Arguments = $"run --project ./{projectName}"
+    };
+    Process.Start(pix)?.WaitForExit();
+}
+
+record Package(string Name, string Version);
