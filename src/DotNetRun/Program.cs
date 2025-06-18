62bd351d21af6eb34b0139367eedc6983e5f0ca7
Sysinfocus <info@sysinfocus.com>
2025-06-18 08:51:58 +0530

    Updated to support multiple .cs files and pass arguments to the executing project.

--- src/DotNetRun/Program.cs
+++ src/DotNetRun/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System.Diagnostics;
 using System.Text;
 
-if (args.Length != 1) return;
+if (args.Length == 0) return;
 
 var temporaryFolder = GetTemporaryFolder();
 
 var file = args[0];
+var passArgs = args.Length > 1 ? string.Join(' ', args[1..]) : null;
+
 if (!File.Exists(file))
 {
     Console.WriteLine($"File '{file}' not found.");
@@ -18,7 +20,7 @@ if (ext != ".cs")
     return;
 }
 
-ReadCSFile(temporaryFolder, args[0]);
+ReadCSFile(temporaryFolder, args[0], passArgs);
 
 Console.WriteLine();
 
@@ -32,33 +34,35 @@ static string GetTemporaryFolder()
     return path;
 }
 
-static void ReadCSFile(string path, string projectFile)
+static void ReadCSFile(string path, string projectFile, string? args = null)
 {    
     var projectName = Path.GetFileNameWithoutExtension(projectFile);
     var programFile = Path.Combine(path, projectName, "Program.cs");
 
     var csproj = Path.Combine(path, projectName, projectName + ".csproj");
-    var csprojContent = File.ReadAllText(csproj);    
 
     if (!File.Exists(csproj))
     {
         var pi = new ProcessStartInfo
         {
             WorkingDirectory = path,
+            RedirectStandardOutput = true,
             UseShellExecute = false,
             CreateNoWindow = true,
             FileName = "dotnet",
             Arguments = $"new console -n {projectName}"
         };
-        Process.Start(pi);
+        Process.Start(pi)?.WaitForExit();
     }
 
+    var csprojContent = File.ReadAllText(csproj);
     var lines = File.ReadAllLines(projectFile);
     HashSet<Package> packages = [];
+    HashSet<Import> imports = [];
     var sb = new StringBuilder();
     foreach (var line in lines)
     {
-        if (line.StartsWith("#:"))
+        if (line.StartsWith("#:package"))
         {
             var package = line.Trim().Replace("#:package ", "").Split('@');
             packages.Add(new(package[0], package[1]));
@@ -77,20 +81,48 @@ static void ReadCSFile(string path, string projectFile)
                 Process.Start(addPackage);
             }
         }
+        else if (line.StartsWith("#:import"))
+        {
+            var import = line.Trim().Replace("#:import ", "");
+            if (import.ToLower() == "*.cs")
+            {
+                var files = Directory.GetFiles(Environment.CurrentDirectory, "*.cs", SearchOption.TopDirectoryOnly);
+                foreach (var file in files)
+                {
+                    if (Path.GetFileName(file) != Path.GetFileName(projectFile) && !imports.Any(i => i.File == file))
+                    {
+                        imports.Add(new(Path.GetFileName(file)));
+                    }
+                }
+            }
+            else if (File.Exists(import))
+            {
+                imports.Add(new(import));
+            }
+        }
         else
         {
             sb.AppendLine(line);
         }
     }
 
+    if (imports.Count > 0)
+    {
+        foreach (var import in imports)
+        {
+            File.Copy(import.File, Path.Combine(path, projectName, import.File), true);
+        }
+    }
+
     File.WriteAllText(programFile, sb.ToString());
-    var pix = new ProcessStartInfo
+    var runProject = new ProcessStartInfo
     {
         WorkingDirectory = path,
         FileName = "dotnet",
-        Arguments = $"run --project ./{projectName}"
+        Arguments = $"run -v q {args} --project ./{projectName}"
     };
-    Process.Start(pix)?.WaitForExit();
+    Process.Start(runProject)?.WaitForExit();
 }
 
 record Package(string Name, string Version);
+record Import(string File);
