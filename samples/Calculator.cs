0b53635a8a5cea5f8aa90f44168d26407f4c7edb
Sysinfocus <109056087+Sysinfocus@users.noreply.github.com>
2025-06-18 09:11:31 +0530

    Samples

---
+++ samples/Calculator.cs
@@ -0,0 +1,7 @@
+public static class Calculator
+{
+    public static void Add(int a, int b)
+    {
+        Console.WriteLine($"Sum of {a}+{b}={a+b}");
+    }
+} 
---
+++ samples/Example.cs
@@ -0,0 +1,7 @@
+public static class Example
+{
+    public static void Test()
+    {
+        Console.WriteLine("This is a test output from another class.");
+    }
+} 
